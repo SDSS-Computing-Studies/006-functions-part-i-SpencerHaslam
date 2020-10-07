72ac47dc6dff9d195fcd6d8da5ac933069256806
SpencerHaslam <71406886+SpencerHaslam@users.noreply.github.com>
2020-10-07 13:51:36 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -8,19 +8,7 @@ def sum(a,b):
     answer = a + b
     return answer
 
-def power(a,b):
-    answer = a**b
-    return answer
 
-def input2Float():
-    a = input("Enter a float:")
-    b = input("Enter another float:")
-    a = float(a)
-    b = float(b)
-    return [a,b]
-
-def output(value):
-    print("The answer is " + str(value))
 #this should return a value of 7
 x = sum(3,4)
 
@@ -28,11 +16,3 @@ x = sum(3,4)
 y = sum(11,1.5)
 print(x)
 print(y)
-
-print("\n\n")
-
-
-
-x,y = input2Float()
-answer = sum(x,y)
-output(answer)
