3a03c987f61c5ecb1dd2226641400bebb8585169
SpencerHaslam <71406886+SpencerHaslam@users.noreply.github.com>
2020-10-07 14:33:31 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -6,3 +6,11 @@ The input is a list.
 The return value is the largest value in the list
 (2 points)
 """
+def largest(list):
+    answer = list.sort()
+    answer = list[-1]
+    return answer
+    
+leest = [3,1,4,7,13,9]
+big = largest(3,1,5,4)
+print (big)
